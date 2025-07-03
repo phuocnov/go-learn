fc97cf8e0669be48940894e86c6b315c01334397
phuocnguyen <phuocnguyen@gmail.com>
2025-07-03 13:18:17 +0700

    install database driver

---
+++ cmd/queries/create_table.sql
@@ -0,0 +1,40 @@
+CREATE TABLE snippets (
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
+  created DATETIME NOT NULL,
+  expires DATETIME NOT NULL
+);
+
+CREATE INDEX idx_snippets_created ON snippets(created);
+
+INSERT INTO snippets (
+  title, content, created, expires
+  ) VALUES ( 
+  'An old silen pond',
+  'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n- Matsuo Basho',
+  UTC_TIMESTAMP(),
+  DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
+);
+
+INSERT INTO snippets (
+  title, content, created, expires
+  ) VALUES ( 
+  'Over the intry forest',
+  'Over the wintry\nforest, winds holw in rage\nwith no leaves to blow.\n\n- Natsume Soseki',
+  UTC_TIMESTAMP(),
+  DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
+);
+
+
+INSERT INTO snippets (
+  title, content, created, expires
+  ) VALUES ( 
+  'In jails',
+  'Inside the jail, no rum nor flower\nGlorious nightsight is not ignoreable\nwith no leaves to blow.\n\n- Natsume Soseki',
+  UTC_TIMESTAMP(),
+  DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
+);
+
+CREATE USER 'web'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON snippetbos.* TO 'web'@'localhost';
