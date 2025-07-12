29fccd64168fe46914a65ad28eb09944947296b8
phuocnguyen <phuocnguyen@gmail.com>
2025-07-12 19:51:54 +0700

    add flash session, also on defalut add session to template for render

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -68,6 +68,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	app.session.Put(r, "flash", "Snippet successfully created!")
 	// Redirect the user tho the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
